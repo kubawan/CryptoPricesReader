e8feb0331c7d77482aee74d6f644ca10199ef664
Kuba Wandachowicz <kuba.wandachowicz@gmail.com>
2022-07-13 21:09:27 +0200

    new branch for test

--- CryptoPricesReader.Data/Models/Responses/IntervalTicker.cs
+++ CryptoPricesReader.Data/Models/Responses/IntervalTicker.cs
@@ -5,24 +5,24 @@ namespace CryptoPricesReader.Data.Models.Responses
     public class IntervalTicker
     {
         [JsonPropertyName("volume")]
-        public string Volume { get; set; }
+        public string? Volume { get; set; }
 
         [JsonPropertyName("price_change")]
-        public string PriceChange { get; set; }
+        public string? PriceChange { get; set; }
 
         [JsonPropertyName("price_change_pct")]
-        public string PriceChangePct { get; set; }
+        public string? PriceChangePct { get; set; }
 
         [JsonPropertyName("volume_change")]
-        public string VolumeChange { get; set; }
+        public string? VolumeChange { get; set; }
 
         [JsonPropertyName("volume_change_pct")]
-        public string VolumeChangePct { get; set; }
+        public string? VolumeChangePct { get; set; }
 
         [JsonPropertyName("market_cap_change")]
-        public string MarketCapChange { get; set; }
+        public string? MarketCapChange { get; set; }
 
         [JsonPropertyName("market_cap_change_pct")]
-        public string MarketCapChangePct { get; set; }
+        public string? MarketCapChangePct { get; set; }
     }
 }
